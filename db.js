8aa0f3c1ea8faa16e68974b15b9a2b17b7563650
Nishant Singh <srnikhil3721@gmail.com>
2021-10-08 14:06:20 +0530

    update changes

--- db.js
+++ db.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
+// const mongoUri =
+//   "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+
 const mongoUri =
-  "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+  "mongodb+srv://Nikhil:Pj6DduWF8iQj1h2y@inotebook.kumwf.mongodb.net/iNotebook?retryWrites=true&w=majority";
 
 const connectToMongo = () => {
   mongoose.connect(process.env.MONGODB_URI || mongoUri, () => {
